62e8d5f525a0f5796dd8f4ededbe8bb39b55dfb6
gosborn <gosborn@gmail.com>
2015-08-04 15:36:05 -0400

    adds user home link to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,7 @@ Rails.application.routes.draw do
 
   root  'challenges#index'
 
-  get '/users/:id', to: 'users#show'
-
+  get '/users/:id', to: 'users#show', as: 'user'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
