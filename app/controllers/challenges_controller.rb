dd62d2d8df213bc13e20a2afb9be5c1d6385eb84
gosborn <gosborn@gmail.com>
2015-08-04 10:39:10 -0400

    adds very basic data to show pages, makes form to input new challenges

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,2 +1,41 @@
 class ChallengesController < ApplicationController
+
+  def index
+    
+
+  end
+
+  def new
+    @challenge = Challenge.new
+  end
+
+  def show
+    @challenge = Challenge.find(params["id"])
+
+  end
+
+
+
+  def create
+    
+    @challenged = User.find_by(email: params[:challenge]["challenged_id"])
+    @challenge = Challenge.new(challenge_params)
+    @challenge.challenged = @challenged
+    @challenge.challenger = current_user
+    @challenge.save
+
+    binding.pry
+
+    render 'show'
+
+  end
+
+  private
+
+  def challenge_params
+    params.require(:challenge).permit(:title, :description, :challenge_duration, :voting_duration)
+
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :votes
 
-  root "home#index"
+  root  'challenges#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
