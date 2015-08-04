7c09b4f2c7a35d0eb6c892d3b05302be8df28b2b
nrosella <ninorosella@outlook.com>
2015-08-04 15:17:35 -0400

    add name field to user sign up, add user show page and begin to build user show page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   root  'challenges#index'
 
+  get '/users/:id', to: 'users#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
