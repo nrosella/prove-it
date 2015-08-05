b7634bd04a646b736cf6b76bdfe3a3249f194404
gosborn <gosborn@gmail.com>
2015-08-05 16:00:11 -0400

    adds winner and loser methods to challenge to easily find

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -8,5 +8,22 @@ class Challenge < ActiveRecord::Base
   has_many :evidences
 
   validates :title, presence: true
-    
+
+  def winner
+    total_votes.max_by{|k,v| v}[0]
+  end
+
+  def loser
+    total_votes.min_by{|k,v| v}[0]
+  end
+
+  def total_votes
+    {}.tap do |results|
+      self.users.each do |user|
+        results[user] = self.votes.where(recipient_id: user.id).size
+      end
+    end 
+  end
+
+
 end
