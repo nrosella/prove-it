e99d7710983ca308830a02f530314f866a0d59a5
gosborn <gosborn@gmail.com>
2015-08-05 22:33:40 -0400

    adds some new partials to render, adds some ajax, cleans up challenges model, methods are hot fire

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,13 +1,14 @@
 class VotesController < ApplicationController
   def create
     @challenge = Challenge.find(params[:vote][:challenge_id])
-    Vote.create (vote_params)
+    Vote.create(vote_params)
     if @challenge.votes.size > 10
       @challenge.status = 'closed'
       @challenge.save
     end
-    redirect_to challenge_path(@challenge)
-
+    respond_to do |format|
+      format.js
+    end
   end
 
   private
