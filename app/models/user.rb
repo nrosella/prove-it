a6a0c01d1a43ff786bea6f05c8d302ddf91133b7
travisje <travisje@gmail.com>
2015-08-11 19:04:33 -0400

    make facebook profile pic save on user instantiation

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,13 +41,13 @@ class User < ActiveRecord::Base
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name
+      user.avatar = open(auth.info.image)
          # assuming the user model has a name
       # user.image = auth.info.image # assuming the user model has an image
     end
   end
 
   def self.new_with_session(params, session)
-    binding.pry
      super.tap do |user|
        if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
          user.email = data["email"] if user.email.blank?
