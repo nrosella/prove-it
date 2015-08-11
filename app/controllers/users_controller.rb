cabddb93b250a5275bd832493decb6540c3326ce
nrosella <ninorosella@outlook.com>
2015-08-11 13:31:01 -0400

    chart.js initialised / integrated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,22 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       @challenges = @user.challenges.where.not(status: 'pending')
     end
-		
+
+	 @challenge_data = 
+    [
+	    {
+	        value: 300,
+	        color:"#F7464A",
+	        highlight: "#FF5A5E",
+	        label: "Red"
+	    },
+	    {
+	        value: 500,
+	        color: "#46BFBD",
+	        highlight: "#5AD3D1",
+	        label: "Green"
+	    }
+  ]
 	end
 
 end
